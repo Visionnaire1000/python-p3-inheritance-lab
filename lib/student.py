a168c5cd41e65fa7de08d468e7defbb87c2abc58
Louis <cheruiyotlouis@gmail.com>
2025-02-18 16:38:37 +0300

    Initial commit

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-from user import User
+from lib.user import User  # Ensure correct import
 
-class Student(User):
-    
-    def learn(self):
-        pass
+class Student(User):  # Inherit from User
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Correct inheritance
+        self.knowledge = []  # Initialize empty knowledge list
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)  # Add new knowledge
